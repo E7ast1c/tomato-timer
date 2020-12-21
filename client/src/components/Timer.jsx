e4b79e39d63d268e43c2503c881cbc8642ab988b
Korolkovart <korolkovar@yandex.ru>
2020-12-22 00:24:12 +0300

    start timer

--- client/src/components/Timer.jsx
+++ client/src/components/Timer.jsx
@@ -1,7 +1,11 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+
+import LinearProgress from '@material-ui/core/LinearProgress';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 
 import Clock from './Clock'
 import ViewClock from './ViewClock'
@@ -13,7 +17,7 @@ const useStyles = makeStyles({
   },
 });
 
-export default function Timer() { 
+export default function Timer() {
   const initial = 0
   const classes = useStyles();
   const [clockEnabled, setClockEnabled] = useState(false)
@@ -33,28 +37,37 @@ export default function Timer() {
 
   return (
     <div>
-    {clockEnabled && !pauseClock && <Clock params={{progress, setProgress}} />}
-    {clockEnabled && pauseClock && <ViewClock params={{progress}} />}
+      { !clockEnabled && 
+      <Box display="flex" alignItems="center" justifyContent="center">
+        <Box width="60%" mr={1}>
+          <LinearProgress variant="determinate" value={0} />
+        </Box>
+        <Box minWidth={35}>
+          <Typography variant="body2" >{"0%"}</Typography>
+        </Box>
+      </Box>}
+      {clockEnabled && !pauseClock && <Clock params={{ progress, setProgress }} />}
+      {clockEnabled && pauseClock && <ViewClock params={{ progress }} />}
       <div>
-        <Button 
-          variant="contained"  
+        <Button
+          variant="contained"
           color="primary"
           onClick={() => start()}
-          >
+        >
           Start
         </Button>
-        <Button 
-          variant="contained" 
-          style={{"backgroundColor": "yellow"}}
+        <Button
+          variant="contained"
+          style={{ "backgroundColor": "yellow" }}
           onClick={() => setPauseClock(true)}
-          >
+        >
           Pause
         </Button>
-        <Button 
-          variant="contained" 
+        <Button
+          variant="contained"
           color="secondary"
           onClick={() => stop()}
-          >
+        >
           Stop
         </Button>
       </div>
